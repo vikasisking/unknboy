cc5da854d9f545193a4b7d68be49b4c6b6846311
vikasisking <vikashgup9793@gmail.com>
2025-10-22 16:09:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ LOGIN_URL = "http://51.89.99.105/NumberPanel/signin"
 XHR_URL = "http://51.89.99.105/NumberPanel/agent/res/data_smscdr.php?fdate1=2025-09-05%2000:00:00&fdate2=2026-09-04%2023:59:59&frange=&fclient=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&sEcho=1&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=2&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1756968295291"
 USERNAME = os.getenv("USERNAME", "Muhammad1222")
 PASSWORD = os.getenv("PASSWORD", "Muhammad1222")
-OTP_GROUP_IDS = ["-10029082859"]  # Updated with multiple group IDs
+OTP_GROUP_IDS = ["-1002721450223"]  # Updated with multiple group IDs
 CHANNEL_LINK = "https://t.me/pakcybernum"
 DEVELOPER_ID = "@Pakcyberotp111bo"
 
@@ -438,7 +438,7 @@ def start(message):
     # Use channel ID for private one, and username for public
     REQUIRED_CHANNELS = [
         "@pakcybernum",  # ← Replace this with your private channel's chat ID
-        ""
+        "-1003147926675"
     ]
 
     # ---- Check Membership ----
